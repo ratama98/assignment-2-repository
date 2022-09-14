b223f2b10a298f09932789b8beccefb34b56a52d
raditya.aditama <raditya.aditama@ui.ac.id>
2022-09-14 15:00:13 +0700

    Assignment finished

--- katalog/urls.py
+++ katalog/urls.py
@@ -1,1 +1,11 @@
+from django.urls import path
+from katalog.views import show_katalog
+
+
 # TODO: Implement Routings Here
+
+app_name = "katalog"
+
+urlpatterns = [
+    path('', show_katalog, name = "show_katalog"),
+]
--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
-# TODO: Create your views here.
+# Create your views here.
+
+def show_katalog(request):
+    data_katalog = CatalogItem.objects.all()
+    
+    context = {
+        "student_nama": "Raditya Aditama",
+        "student_id": "2106750313",
+        "list": data_katalog
+    }
+
+    return render(request, "katalog.html", context)
