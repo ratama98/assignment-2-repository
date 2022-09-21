04cb7c021ac74df938870e3d67021c0ebaaa909e
raditya.aditama <raditya.aditama@ui.ac.id>
2022-09-22 00:56:15 +0700

    Update settings and tests

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,15 +1,21 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 # Create your tests here.
 class WatchlistTestCase(TestCase):
-    def html_test(self):
-        response = self.client.get('/mywatchlist/html/')
+    def setUp(self):
+        self.client = Client()
+        self.html = "/mywatchlist/html/"
+        self.xml = "/mywatchlist/xml/"
+        self.json = "/mywatchlist/json/"
+
+    def test_html(self):
+        response = self.client.get(self.html)
         self.assertEqual(response.status_code, 200)
 
-    def xml_test(self):
-        response = self.client.get('/mywatchlist/xml/')
+    def test_xml(self):
+        response = self.client.get(self.xml)
         self.assertEqual(response.status_code, 200)
 
-    def json_test(self):
-        response = self.client.get('/mywatchlist/json/')
+    def test_json(self):
+        response = self.client.get(self.json)
         self.assertEqual(response.status_code, 200)
