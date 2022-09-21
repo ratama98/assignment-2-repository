05b7f0d633f03404885d79e3795841fc94fc3e76
raditya.aditama <raditya.aditama@ui.ac.id>
2022-09-22 01:18:07 +0700

    Finished bonus task

--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -6,11 +6,21 @@ from django.core import serializers
 # Create your views here.
 def show_html(request):
     data_mywatchlist = MyWatchList.objects.all()
+
+    number_watched = 0
+    not_watched = 0
+    for item in data_mywatchlist:
+        if (item.watched):
+            number_watched += 1
+        else:
+            not_watched += 1
     
     context = {
         "student_nama": "Raditya Aditama",
         "student_id": "2106750313",
-        "list": data_mywatchlist
+        "list": data_mywatchlist ,
+        "number_watched": number_watched,
+        "not_watched": not_watched
     }
 
     return render(request, "mywatchlist.html", context)
